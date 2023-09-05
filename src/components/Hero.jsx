6463ed3113ca528b65225d8802f5522463b6d479
BAD-EiZA <badapplestd@gmail.com>
2023-09-05 16:10:47 +0700

    New UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           className="w-28 object-contain"
         />
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-outline btn-neutral m-1">
+          <label tabIndex={0} className="btn btn-outline btn-primary m-1">
             Other Project
           </label>
           <ul
@@ -19,7 +19,7 @@ const Hero = () => {
           >
             <div className="p-2 flex flex-col justify-center items-center">
               <div className="avatar py-2">
-                <div className="w-10 rounded-full">
+                <div className="rounded-full w-14">
                   <img src="https://i.imgur.com/HbGPpYt.png" />
                 </div>
               </div>
@@ -35,69 +35,67 @@ const Hero = () => {
         </div>
       </nav>
       <div className="h-[468px] grid grid-cols-2 mt-10 items-center">
-        <div className=" h-full flex items-center">
-          <h1 className=" text-4xl font-extrabold  text-black sm:text-6xl text-centercapitalize">
-            Get An Outline Of The Article With{" "}
-            <span className="text-slate-800">MavisAI</span>
+        <div className=" h-full grid grid-rows-1  items-center">
+          <h1 className=" text-4xl font-semibold   text-black sm:text-7xl text-centercapitalize">
+            Get an <span className="font-extrabold">Outline</span> of an Article
+            with <span className="mavisai">MavisAI</span>
           </h1>
+          <h2 className="font-semibold text-2xl ">Technology of</h2>
+          <div className=" w-full mb-10  flex flex-row mt-2 items-center gap-10 justify-between">
+            
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg"
+              className="w-10 h-10"
+              alt=""
+            />
+            <img
+              src="https://freelogopng.com/images/all_img/1681038242chatgpt-logo-png.png"
+              className="w-10 h-10"
+              alt=""
+            />
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
+              className="w-10 h-10"
+              alt=""
+            />
+
+            <img
+              src="https://raw.githubusercontent.com/saadeghi/daisyui/master/src/docs/static/images/daisyui-logo/favicon-192.png"
+              className="w-10 h-10"
+              alt=""
+            />
+
+            <img
+              src="https://avatars.githubusercontent.com/u/67109815?s=280&v=4"
+              className="w-10 h-10"
+              alt=""
+            />
+          </div>
         </div>
         <div className=" h-full">
           <img
-            src="https://i.imgur.com/UeOVZDF.png"
+            src="https://i.imgur.com/Rg8jaxT.png"
             alt=""
-            className="rounded-full h-full"
+            className=" h-full"
           />
         </div>
       </div>
 
-      <div className=" w-full border-b pb-4 flex flex-row mt-8 items-center gap-10 justify-between">
-        <h2 className="font-semibold text-2xl">TECHNOLOGY</h2>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg"
-          className="w-10 h-10"
-          alt=""
-        />
-        <img
-          src="https://freelogopng.com/images/all_img/1681038242chatgpt-logo-png.png"
-          className="w-10 h-10"
-          alt=""
-        />
-        <img
-          src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
-          className="w-10 h-10"
-          alt=""
-        />
-
-        <img
-          src="https://raw.githubusercontent.com/saadeghi/daisyui/master/src/docs/static/images/daisyui-logo/favicon-192.png"
-          className="w-10 h-10"
-          alt=""
-        />
-
-        <img
-          src="https://avatars.githubusercontent.com/u/67109815?s=280&v=4"
-          className="w-10 h-10"
-          alt=""
-        />
-      </div>
-
       <div className="">
-        <h1 className="mt-2 text-3xl font-extrabold leading-[1.15] text-black sm:text-4xl text-center">
+        <h1 className="mt-2 text-3xl font-bold leading-[1.15] text-black sm:text-4xl text-center">
           Outline Your Article
         </h1>
         <h2 className="desc">
-          Create an outline in the article with the help of MavisAI so it's
-          easier~
+        Create an outline for an article with the help of MavisAI
         </h2>
       </div>
-      <div className="form-control w-full max-w-2xl mt-2">
-        
+      {/* <div className="form-control w-full max-w-2xl mt-2">
         <input
           type="text"
           placeholder="Input your article URL here..."
           className="input input-bordered w-full max-w-2xl"
         />
-      </div>
+      </div> */}
     </header>
     // #181736
   );
